739421a8c4d47e91294b51f637d00767565ebd2f
AnnSo-B <as.baujard.33@gmail.com>
2020-05-06 20:00:22 +0200

    handle the task's completion on click on validate button

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -4,15 +4,12 @@ const app = {
 
     // get the list of the tasks
     const taskList = document.querySelectorAll('.task--todo, .task--complete, .task--done, .task--edit, .task--archive');
-    console.log(taskList);
 
     // for each task we'll add a listener on the validate button
     for (let taskIndex = 0; taskIndex < taskList.length ; taskIndex++) {
       let task = taskList[taskIndex];
-      console.log(task);
       // we select the validate button 
       let validateButton = task.querySelector('.task__content__button__validate');
-      console.log(validateButton);
       // we had the handler
       validateButton.addEventListener('click', app.handleValidateButton)
     }
@@ -22,7 +19,16 @@ const app = {
    * handler on validate button
    */
   handleValidateButton: function() {
-    console.log('validate button on click')
+    // find the task associated with the validate button
+    let currentTask = event.currentTarget.closest('.task');
+    // change its classes so it becomes a completed task
+    currentTask.classList.remove('task--todo');
+    currentTask.classList.add('task--done');
+
+    // find the progress bar to fill it
+    let currentTaskProgressBar = currentTask.querySelector('.progress-bar');
+    // change its completion percentage
+    currentTaskProgressBar.style.width = '100%';
   }
 }
 
