54653695ead39a3184fb4445052903451514f70a
AnnSo-B <as.baujard.33@gmail.com>
2020-05-10 17:41:38 +0200

    Done - tasks through redux - task to Task component

--- react/src/components/TaskList/Task.js
+++ react/src/components/TaskList/Task.js
@@ -8,30 +8,51 @@ import TaskTitleInput from '../TaskTitleInput';
 import StatusButton from './StatusButton';
 
 // component
-const Task = ({ statusButtons }) => (
-  <article className="task task--todo">
-    <div className="task__content">
-      <div className="task__content__title">
-        <TaskTitleInput value="Acheter les billets d'avion" />
-        <p className="task__content__p">Acheter les billets d'avion</p>
+const Task = ({ title, completion, status, category, statusButtons }) => {
+  // define class according to status
+  let statusClass = '';
+  switch (status) {
+    case 2:
+      statusClass = 'task--done';
+      break;
+    case 3:
+      statusClass = 'task--archive';
+      break;
+    default: 
+      statusClass = 'task--todo'; // status 1
+  };
+
+  return (
+    <article className={`task ${statusClass}`}>
+      <div className="task__content">
+        <div className="task__content__title">
+          <TaskTitleInput value={title} />
+          <p className="task__content__p">{title}</p>
+        </div>
+        <div className="task__content__category">
+          <p className="task__content__category__p">{category.name}</p>
+        </div>
+        <div className="task__content__buttons">
+          {
+            statusButtons.map((button) => (
+              <StatusButton key={button.id} {...button} />
+            ))
+          }
+        </div>
       </div>
-      <div className="task__content__category">
-        <p className="task__content__category__p">Vacances</p>
-      </div>
-      <div className="task__content__buttons">
-        {
-          statusButtons.map((button) => (
-            <StatusButton key={button.id} {...button} />
-          ))
-        }
-      </div>
-    </div>
-    <ProgressBar now={25} />
-  </article>
-);
+      <ProgressBar now={completion} />
+    </article>
+  )
+};
 
 // Props validation
 Task.propTypes = {
+  title: PropTypes.string.isRequired,
+  completion: PropTypes.number.isRequired,
+  status: PropTypes.number.isRequired,
+  category: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }),
   statusButtons: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
