0881cfe380347d6025bf61a23b9e4080e1c9f041
AnnSo-B <as.baujard.33@gmail.com>
2020-05-10 15:16:48 +0200

    static AddTaskForm Component

--- react/src/components/AddTaskForm/index.js
+++ react/src/components/AddTaskForm/index.js
@@ -2,11 +2,24 @@
 import React from 'react';
 
 // local imports
+import CategoryMenu from '../CategoryMenu';
 
 // component
 const AddTaskForm = () => (
   <section className="task task--add">
-    AddTaskForm
+    <form class="task--add__form">
+      <div class="task__content">
+        <div class="task__content__title">
+          <input class="task__content__input" type="text" name="title" placeholder="Nom de la tâche" />
+        </div>
+      </div>
+      <div id="task--add__category-select" class="task-form-group my-3 selectCategoryMenu">
+        <CategoryMenu />
+      </div>
+      <button type="submit" class="btn btn-primary task--add__button">
+        <span>+</span> Ajouter
+      </button>
+    </form>
   </section>
 );
 
