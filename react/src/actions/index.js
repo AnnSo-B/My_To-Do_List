73c85f2dc85a3b6392f6d08c2133229b96a0924e
AnnSo-B <as.baujard.33@gmail.com>
2020-05-13 10:27:12 +0200

    task deletion

--- react/src/actions/index.js
+++ react/src/actions/index.js
@@ -7,6 +7,9 @@ export const FETCH_TASK_LIST_ERROR = 'FETCH_TASK_LIST_ERROR';
 export const TASK_UPDATE = 'TASK_UPDATE';
 export const TASK_UPDATE_SUCCESS = 'TASK_UPDATE_SUCCESS';
 export const TASK_UPDATE_ERROR = 'TASK_UPDATE_ERROR';
+export const TASK_DELETION = 'TASK_DELETION';
+export const TASK_DELETION_SUCCESS = 'TASK_DELETION_SUCCESS';
+export const TASK_DELETION_ERROR = 'TASK_DELETION_ERROR';
 // Categories
 export const FETCH_CATEGORY_LIST = 'FETCH_CATEGORY_LIST';
 export const FETCH_CATEGORY_LIST_SUCCESS = 'FETCH_CATEGORY_LIST_SUCCESS';
@@ -42,6 +45,21 @@ export const taskUpdateSuccess = (payload) => ({
 
 export const taskUpdateError = (payload) => ({
   type: TASK_UPDATE_ERROR,
+  payload,
+});
+
+export const taskDeletion = (payload) => ({
+  type: TASK_DELETION,
+  payload,
+});
+
+export const taskDeletionSuccess = (payload) => ({
+  type: TASK_DELETION_SUCCESS,
+  payload,
+});
+
+export const taskDeletionError = (payload) => ({
+  type: TASK_DELETION_ERROR,
   payload,
 });
 
